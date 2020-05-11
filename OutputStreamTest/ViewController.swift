c22dc5f692e0b9bb10178f61e5be265d3e942dd9
guitarmike1966 <guitarmike1966@gmail.com>
2020-05-10 20:00:59 -0500

    FileHandle test

--- OutputStreamTest/ViewController.swift
+++ OutputStreamTest/ViewController.swift
@@ -14,6 +14,32 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+//        let stream = OutputStream(toFileAtPath: "~/Documents/logtest.txt", append: false)
+//
+//        stream.close()
+
+        guard let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Directory does not exist")
+            return
+        }
+
+        let fileUrl = directory.appendingPathComponent("logtest.txt")
+
+        let fileHandle = FileHandle(forWritingAtPath: fileUrl.path)
+
+        if fileHandle != nil {
+            let data = ("Silentium est aureum" as NSString).data(using: String.Encoding.utf8.rawValue)
+
+            // Write it to the file
+            fileHandle?.write(data!)
+
+            fileHandle?.closeFile()
+        }
+        else {
+            print("File open failed")
+        }
+
     }
 
     override var representedObject: Any? {
