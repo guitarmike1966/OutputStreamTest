6095a197f33480b45bc29c859e8772039ec748c3
guitarmike1966 <guitarmike1966@gmail.com>
2020-05-10 19:06:11 -0500

    Initial Commit

---
+++ OutputStreamTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OutputStreamTest
+//
+//  Created by mikey on 5/10/20.
+//  Copyright © 2020 Sand Creek Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
